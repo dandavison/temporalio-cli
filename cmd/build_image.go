411f29599494548a035e9591d9d30d0af3f26a27
Dan Davison <dan.davison@temporal.io>
2025-04-11 22:51:15 -0400

    build image

--- cmd/build_image.go
+++ cmd/build_image.go
@@ -1,4 +1,4 @@
-package main
+package cmd
 
 import (
 	"context"
@@ -11,7 +11,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"github.com/temporalio/omes/cmd/cmdoptions"
+	"go.temporal.io/sdk/log"
 	"go.uber.org/zap"
 	"golang.org/x/mod/semver"
 )
@@ -19,8 +19,8 @@ import (
 func buildWorkerImageCmd() *cobra.Command {
 	var b workerImageBuilder
 	cmd := &cobra.Command{
-		Use:   "build-worker-image",
-		Short: "Build an image",
+		Use:   "build-docker-image",
+		Short: "Build docker image",
 		Run: func(cmd *cobra.Command, args []string) {
 			if err := b.build(cmd.Context()); err != nil {
 				b.logger.Fatal(err)
@@ -28,111 +28,66 @@ func buildWorkerImageCmd() *cobra.Command {
 		},
 	}
 	b.addCLIFlags(cmd.Flags())
-	cmd.MarkFlagRequired("language")
 	cmd.MarkFlagRequired("version")
 	return cmd
 }
 
 type workerImageBuilder struct {
-	logger         *zap.SugaredLogger
-	language       string
-	version        string
-	tagAsLatest    bool
-	platform       string
-	imageName      string
-	dryRun         bool
-	saveImage      string
-	tags           []string
-	labels         []string
-	loggingOptions cmdoptions.LoggingOptions
+	logger    *zap.SugaredLogger
+	version   string
+	platform  string
+	imageName string
+	dryRun    bool
+	saveImage string
+	tags      []string
+	labels    []string
 }
 
 func (b *workerImageBuilder) addCLIFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&b.language, "language", "", "Language to build a worker image for")
 	fs.StringVar(&b.version, "version", "",
 		"SDK version to build a worker image for - treated as path if slash present, but must be beneath this dir and at least one tag required")
-	fs.BoolVar(&b.tagAsLatest, "tag-as-latest", false,
-		"If set, tag the image as latest in addition to the omes commit sha tag")
 	fs.StringVar(&b.platform, "platform", "", "Platform for use in docker build --platform")
-	fs.StringVar(&b.imageName, "image-name", "omes", "Name of the image to build")
+	// TODO(dan): image-name ?
+	fs.StringVar(&b.imageName, "image-name", "temporal", "Name of the image to build")
 	fs.BoolVar(&b.dryRun, "dry-run", false, "If set, just print the commands that would run but do not run them")
 	fs.StringSliceVar(&b.tags, "image-tag", nil, "Additional tags to add to the image")
 	fs.StringSliceVar(&b.labels, "image-label", nil, "Additional labels to add to the image")
 	fs.StringVar(&b.saveImage, "save-image", "", "If set, will run `docker save` on the produced image, saving it to the provided path")
-	b.loggingOptions.AddCLIFlags(fs)
 }
 
 func (b *workerImageBuilder) build(ctx context.Context) error {
-	b.logger = b.loggingOptions.MustCreateLogger()
-	lang, err := normalizeLangName(b.language)
-	if err != nil {
-		return err
-	}
-	// At some point we probably want to replace this with a meaningful version of omes itself
-	omesVersion, err := getCurrentCommitSha(ctx)
-	if err != nil {
-		return err
-	}
 
-	// Setup args, tags, and labels based on version
-	isPathVersion := strings.ContainsAny(b.version, `/\`)
 	var buildArgs []string
-	if isPathVersion {
-		if len(b.tags) == 0 {
-			return fmt.Errorf("at least one tag required for path version")
-		} else if s, err := os.Stat(b.version); err != nil {
-			return fmt.Errorf("invalid path version: %w", err)
-		} else if !s.IsDir() {
-			return fmt.Errorf("invalid path version: must be dir")
-		} else if !filepath.IsLocal(b.version) {
-			return fmt.Errorf("invalid path version: must be beneath this dir")
-		}
-		// Dockerfile copies entire version path to ./repo
-		buildArgs = append(buildArgs, "SDK_VERSION=./repo", "SDK_DIR="+b.version)
-	} else {
-		buildArgs = append(buildArgs, "SDK_VERSION="+b.version)
-		// Add label for version
-		b.addLabelIfNotPresent("io.temporal.sdk.version", b.version)
-		// Check for valid version
-		versionToCheck := b.version
-		if !strings.HasPrefix(b.version, "v") {
-			versionToCheck = "v" + versionToCheck
-		}
-		if semver.Canonical(versionToCheck) == "" {
-			return fmt.Errorf("expected valid semver")
-		}
-		// Add tag for lang-fullsemver without leading "v". We are intentionally
-		// including semver build metadata.
-		langTagComponent := lang + "-" + strings.TrimPrefix(b.version, "v")
-		b.tags = append(b.tags, omesVersion+"-"+langTagComponent)
-		if b.tagAsLatest {
-			b.tags = append(b.tags, langTagComponent)
-			b.tags = append(b.tags, lang+"-latest")
-		}
+	versionToCheck := b.version
+	if !strings.HasPrefix(b.version, "v") {
+		versionToCheck = "v" + versionToCheck
 	}
+	if semver.Canonical(versionToCheck) == "" {
+		return fmt.Errorf("expected valid semver")
+	}
+	// Add tag for lang-fullsemver without leading "v". We are intentionally
+	// including semver build metadata.
+	b.tags = append(b.tags, b.version)
 
-	// Setup additional labels
 	gitRef, err := gitRef(ctx, ".git")
 	if err != nil {
 		return err
 	}
 	b.addLabelIfNotPresent("org.opencontainers.image.created", time.Now().UTC().Format(time.RFC3339))
-	b.addLabelIfNotPresent("org.opencontainers.image.source", "https://github.com/temporalio/omes")
+	b.addLabelIfNotPresent("org.opencontainers.image.source", "https://github.com/temporalio/cli")
 	b.addLabelIfNotPresent("org.opencontainers.image.vendor", "Temporal Technologies Inc.")
 	b.addLabelIfNotPresent("org.opencontainers.image.authors", "Temporal SDK team <sdk-team@temporal.io>")
 	b.addLabelIfNotPresent("org.opencontainers.image.licenses", "MIT")
 	b.addLabelIfNotPresent("org.opencontainers.image.revision", gitRef)
-	b.addLabelIfNotPresent("org.opencontainers.image.title", "Load testing for "+lang)
-	b.addLabelIfNotPresent("org.opencontainers.image.documentation", "See README at https://github.com/temporalio/omes")
-	b.addLabelIfNotPresent("io.temporal.sdk.name", lang)
-	b.addLabelIfNotPresent("io.temporal.sdk.version", b.version)
-	b.addLabelIfNotPresent("io.temporal.omes.githash", omesVersion)
+	b.addLabelIfNotPresent("org.opencontainers.image.title", "Temporal CLI")
+	b.addLabelIfNotPresent("org.opencontainers.image.documentation", "See README at https://github.com/temporalio/cli")
+	b.addLabelIfNotPresent("io.temporal.cli.version", b.version)
 
 	// Prepare docker command args
 	args := []string{
 		"build",
 		"--pull",
-		"--file", "dockerfiles/" + lang + ".Dockerfile",
+		"--file", ".Dockerfile",
 	}
 	if b.platform != "" {
 		args = append(args, "--platform", b.platform, "--build-arg", "PLATFORM="+b.platform)
